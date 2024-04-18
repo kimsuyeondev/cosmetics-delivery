4b8e27cf9bfd4f9535a4467191972ca331c0be7e
suyeon_dev <52491180+kimsuyeondev@users.noreply.github.com>
2024-04-19 00:25:34 +0900

    - #10 의존성 분리 - [이슈4,5 개선] 필요없는 부분 삭제 out,쓰지않는파일

--- module-api/src/main/java/com/cosmetics/CosmeticsApplication.java
+++ module-api/src/main/java/com/cosmetics/CosmeticsApplication.java
@@ -3,7 +3,7 @@ package com.cosmetics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.cosmetics")
+@SpringBootApplication
 public class CosmeticsApplication {
 
 	public static void main(String[] args) {
