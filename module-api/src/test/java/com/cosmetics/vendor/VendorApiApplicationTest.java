51252e0ad978f28a42506e356b0fa115c23faf68
kimsuyeondev <rhaehl@naver.com>
2024-05-22 03:51:50 +0900

    #30 테스트

--- module-api/src/test/java/com/cosmetics/vendor/VendorApiApplicationTest.java
+++ module-api/src/test/java/com/cosmetics/vendor/VendorApiApplicationTest.java
@@ -1,7 +1,6 @@
 package com.cosmetics.vendor;
 
 import com.cosmetics.domain.vendor.dto.VendorManagement;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
