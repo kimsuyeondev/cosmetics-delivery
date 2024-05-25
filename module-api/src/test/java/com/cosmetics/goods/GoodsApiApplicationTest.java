f2f5d891dd17cf2bd5bfe141525bbdb2ba47d250
kimsuyeondev <rhaehl@naver.com>
2024-05-25 15:53:33 +0900

    #30 mysql 설정수정

--- module-api/src/test/java/com/cosmetics/goods/GoodsApiApplicationTest.java
+++ module-api/src/test/java/com/cosmetics/goods/GoodsApiApplicationTest.java
@@ -31,7 +31,7 @@ public class GoodsApiApplicationTest {
     private TestRestTemplate testRestTemplate;
 
     private static GoodsManagementRequest requestGoods() {
-        //item dddddaaas
+        //item ddddds
         List<GoodsItemManagementRequest> items = new ArrayList<>();
 
         items.add(GoodsItemManagementRequest.builder()
